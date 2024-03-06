97b8881db5a81e5e00b39f67bbef66470849039a
msdsn <devsanms@outlook.com>
2024-03-06 19:50:18 +0300

    Starter Kit

--- account/apps.py
+++ account/apps.py
@@ -3,7 +3,7 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'users'
+    name = 'account'
 
     def ready(self) -> None:
-        import users.signals
+        import account.signals
