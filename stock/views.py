6fc35733ae9703413f5901771ad4c798ed9be044
msdsn <devsanms@outlook.com>
2024-03-07 16:20:45 +0300

    endpointe django izinler eklendi

--- stock/views.py
+++ stock/views.py
@@ -2,6 +2,7 @@ from rest_framework import viewsets, filters
 from .models import Category, Brand, Product, Firm, Purchases, Sales
 from .serializers import CategorySerializer, CategoryProductSerializer
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import DjangoModelPermissions
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
@@ -9,6 +10,7 @@ class CategoryViewSet(viewsets.ModelViewSet):
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
     search_fields = ['name']
     filterset_fields = ['name']
+    permission_classes = [DjangoModelPermissions]
 
     def get_serializer_class(self):
         if self.request.query_params.get('name'):
