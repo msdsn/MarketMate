09c890e23267771c453b4c01266f7b8aa3c0b239
msdsn <devsanms@outlook.com>
2024-03-12 17:32:00 +0300

    purchase delete edildiğinde product'ı güncelle

--- stock/views.py
+++ stock/views.py
@@ -87,6 +87,16 @@ class PurchasesView(viewsets.ModelViewSet):
             instance._prefetched_objects_cache = {}
 
         return Response(serializer.data)
+    
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        # decrease stock
+        product = Product.objects.get(id=instance.product_id)
+        product.stock -= instance.quantity
+        product.save()
+        # --------------
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 
